d035eb4db6449e4a1abbd892ebee550a9f5e590f
Alexander Gattin <xrgtn@yandex.ru>
2011-11-25 10:52:00 +0200

    added short description, copyright notice and GPLv3 copying permissions

--- dvdread.c
+++ dvdread.c
@@ -1,3 +1,19 @@
+/*-
+ * Dumps decrypted image of DVD to stdout.
+ * Copyright (c) 2011 Alexander Gattin (xrgtn).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License (<http://www.gnu.org/licenses/>)
+ * for more details.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
